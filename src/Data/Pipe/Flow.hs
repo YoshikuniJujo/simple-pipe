5cee7d447a453c62017af33f80b9a4dedf5d679e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-23 08:43:36 +0900

    use unless in before

--- src/Data/Pipe/Flow.hs
+++ src/Data/Pipe/Flow.hs
@@ -1,11 +1,11 @@
 module Data.Pipe.Flow (before, until) where
 
 import Prelude hiding (until)
+import Control.Monad
 import Data.Pipe
 
 before :: (PipeClass p, Monad m, Monad (p a a m)) => (a -> Bool) -> p a a m ()
-before p = (await >>=) . maybe (return ()) $ \x ->
-	if p x then return () else yield x >> before p
+before p = await >>= maybe (return ()) (\x -> unless (p x) $ yield x >> before p)
 
 until :: (PipeClass p, Monad m, Monad (p a a m)) => (a -> Bool) -> p a a m ()
 until p = (await >>=) . maybe (return ()) $ \x ->
