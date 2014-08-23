5c080ef1b313f4200361267d11e5ec29309fb5be
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-23 13:37:22 +0900

    #18 add debug

--- src/Data/Pipe/IO.hs
+++ src/Data/Pipe/IO.hs
@@ -1,32 +1,40 @@
 {-# LANGUAGE FlexibleContexts, ScopedTypeVariables, PackageImports #-}
 
-module Data.Pipe.IO (fromHandle, toHandle, fromFile, toFile) where
+module Data.Pipe.IO (
+	fromHandle, toHandle, fromFile, toFile, debug) where
 
 import Control.Applicative
 import "monads-tf" Control.Monad.Trans
 import Control.Monad.Trans.Control
+import Control.Monad.Base
 import Control.Exception (catch)
 import Data.Pipe
 import System.IO
 import GHC.IO.Exception
 
-fromHandle :: (PipeClass p, Monad m, MonadIO (p i Char m)) =>
+fromHandle :: (PipeClass p, Monad m, MonadBaseControl IO (p i Char m)) =>
 	Handle -> p i Char m ()
 fromHandle h = do
-	mc <- liftIO $ (Just <$> hGetChar h) `catch` \(e :: IOException) ->
+	mc <- liftBase $ (Just <$> hGetChar h) `catch` \(e :: IOException) ->
 		case ioe_type e of
 			EOF -> return Nothing
 			_ -> ioException e
 	maybe (return ()) ((>> fromHandle h) . yield) mc
 
-toHandle :: (PipeClass p, Monad m, MonadIO (p Char o m)) =>
+toHandle :: (PipeClass p, Monad m, MonadBaseControl IO (p Char o m)) =>
 	Handle -> p Char o m ()
-toHandle h = await >>= maybe (return ()) ((>> toHandle h) . liftIO . hPutChar h)
+toHandle h = await >>= maybe (return ()) ((>> toHandle h) . liftBase . hPutChar h)
 
-fromFile :: (PipeClass p, MonadIO m, MonadBaseControl IO m,
-	MonadTrans (p i Char), MonadIO (p i Char m)) => FilePath -> p i Char m ()
-fromFile fp = bracket (liftIO $ openFile fp ReadMode) (liftIO . hClose) fromHandle
+fromFile :: (PipeClass p, MonadBaseControl IO m,
+	MonadTrans (p i Char), MonadBaseControl IO (p i Char m)) =>
+	FilePath -> p i Char m ()
+fromFile fp =
+	bracket (liftBase $ openFile fp ReadMode) (liftBase . hClose) fromHandle
 
 toFile :: (PipeClass p, MonadIO m, MonadBaseControl IO m,
-	MonadTrans (p Char o), MonadIO (p Char o m)) => FilePath -> p Char o m ()
-toFile fp = bracket (liftIO $ openFile fp WriteMode) (liftIO . hClose) toHandle
+	MonadTrans (p Char o), MonadBaseControl IO (p Char o m)) =>
+	FilePath -> p Char o m ()
+toFile fp = bracket (liftBase $ openFile fp WriteMode) (liftBase . hClose) toHandle
+
+debug :: (PipeClass p, Monad m, MonadBaseControl IO (p a a m), Show a) => p a a m ()
+debug = await >>= maybe (return ()) (\x -> liftBase (print x) >> yield x >> debug)
